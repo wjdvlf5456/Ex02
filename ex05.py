68eba0e9cd71ee07f4ca637657fe15a992c532af
최정필 <wjdvlf1407@gmail.com>
2022-07-19 10:20:04 +0900

    --

---
+++ ex05.py
@@ -0,0 +1,30 @@
+#언패킹
+
+t = (10, 20, 30 , "python")
+print(t)
+print(type(t))
+
+a, b, c, d = t
+print(a, b, c, d)
+
+
+aList = [10, 20, 30 , "python"]
+
+h, i, j, k= aList
+print(h, i, j, k)
+
+print("==========================")
+t = (1,2,3)
+print(t, type(t))
+
+
+s=set(t)
+print(s, type(s))
+
+l=list(s)
+print(l, type(l))
+
+tu = tuple(l)
+print(tu, type(tu))
+
+
---
+++ ex06.py
@@ -0,0 +1,21 @@
+a = "orange"
+b = "banana"
+c = "apple"
+
+print("orange" + "banana" + "apple" )
+print("orange"  "banana"  "apple" )
+
+print(a + b + c )
+#print(a b c )
+
+print(a, b, c)
+print("orange","banana","apple" )
+
+print("orange","banana","apple", sep=",")
+print("orange","banana","apple", end="==============\n")
+print("orange","banana","apple", end="\n")
+
+
+print("orange","banana","apple", sep=" ", end="\n")
+
+print("orange","banana","apple", sep=",", end="========")
